81d60281c5affcedb4f7d07211e98a91241c086c
noorqidam <noorqidam@gmail.com>
2021-03-17 11:44:22 +0700

    add midtransHandler and helpers createPremiumAccess

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('orders', [OrderController::class, 'create']);
 Route::get('orders', [OrderController::class, 'index']);
+Route::post('webhook', [WebhookController::class, 'midtransHandler']);
